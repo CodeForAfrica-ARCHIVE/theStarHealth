ab968270658854d6edac92c1b616b8c8647c6970
Nick Hargreaves(1337x) <hargreaves1234@gmail.com>
2013-05-16 16:56:32 +0300

    Removed old files

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$link = mysql_connect('localhost', 'root', 'root');
-	mysql_select_db('health');
-	echo mysql_error($link);
-$home2 = 'http://health.the-star.co.ke/';
-$home = 'http://health.the-star.co.ke/';
-
-?>
