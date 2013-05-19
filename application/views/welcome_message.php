8273afe065fbbb34d7f83134f33837e2f23410ac
Nick Hargreaves(1337x) <hargreaves1234@gmail.com>
2013-05-19 11:05:32 +0300

    sidebar widgets

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,1 +1,60 @@
-hello world
+<div class="container">
+	<div class="row-fluid">
+		<div class="span3">
+			<div class="sidebar_widget">
+				<h4>Backstory</h4>
+			</div>
+			<div class="sidebar_widget">
+				<h5>The story so far</h5>
+			</div>
+			<div class="sidebar_widget">
+				<h5>Evidence Dossier</h5>
+			</div>
+		</div>
+		<div class="span6">
+			<div id="myCarousel" class="carousel slide">
+                <ol class="carousel-indicators">
+                  <li data-target="#myCarousel" data-slide-to="0" class=""></li>
+                  <li data-target="#myCarousel" data-slide-to="1" class=""></li>
+                  <li data-target="#myCarousel" data-slide-to="2" class="active"></li>
+                </ol>
+                <div class="carousel-inner">
+                  <div class="item">
+                    <img src="assets/img/bootstrap-mdo-sfmoma-01.jpg" alt="">
+                    <div class="carousel-caption">
+                      <h4>First Thumbnail label</h4>
+                      <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                    </div>
+                  </div>
+                  <div class="item">
+                    <img src="assets/img/bootstrap-mdo-sfmoma-02.jpg" alt="">
+                    <div class="carousel-caption">
+                      <h4>Second Thumbnail label</h4>
+                      <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                    </div>
+                  </div>
+                  <div class="item active">
+                    <img src="assets/img/bootstrap-mdo-sfmoma-03.jpg" alt="">
+                    <div class="carousel-caption">
+                      <h4>Third Thumbnail label</h4>
+                      <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                    </div>
+                  </div>
+                </div>
+                <a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
+                <a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
+              </div>
+		</div>
+		<div class="span3">
+			<div class="sidebar_widget">
+				<h4>Help Desk</h4>
+			</div>
+			<div class="sidebar_widget">
+				<h5>Help lines + support groups</h5>
+			</div>
+			<div class="sidebar_widget">
+				<h5>Other Resources</h5>
+			</div>
+		</div>
+	</div>
+</div>
