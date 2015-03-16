8b808bafe0af9f77b677f7d669ddccebce42f964
nickhargreaves <hargreaves1234@gmail.com>
2015-03-16 15:36:32 +0300

    save post Alchemy processed localy

--- assets/alchemy_processing.php
+++ assets/alchemy_processing.php
@@ -7,17 +7,21 @@ $feed_url = "http://www.the-star.co.ke/star-health";
 $feed = json_decode(file_get_contents($feed_url, true));
 $items = $feed->nodes;
 
+$articles = array();
 $concepts = array();
 
 foreach($items as $d){
+
     $item = $d->node;
     $response = $alchemyapi->concepts('text',$item->body, null);
+
+    $tags = array();
 
     if ($response['status'] == 'OK') {
 
         foreach ($response['concepts'] as $concept) {
 
-            if (!array_key_exists($concept['text'], $concepts))
+            if (!array_key_exists($concept['text'], $tags))
             {
                 //No exact match
                     //check if this key is substring of existing key or vice versa
@@ -28,30 +32,38 @@ foreach($items as $d){
                         $new_value = $concepts[$existing_key] + 1;
                         $concepts[$existing_key] = $new_value;
 
+                        $tags[] = $existing_key;
+
                     }else{
 
                         $concepts[$concept['text']] = 1;
+
+                        $tags[] = $concept['text'];
                     }
-                    //check for closes levenshtein match
+                    //TODO: check for close levenshtein matches?
             }
             else
             {
                 $new_value = $concepts[$concept['text']] + 1;
                 $concepts[$concept['text']] = $new_value;
+
+                $tags[] = $concept['text'];
             }
 
         }
 
     } else {
-        echo 'Error in the concept tagging call: ', $response['statusInfo'];
+        //echo 'Error in the concept tagging call: ', $response['statusInfo'];
     }
+
+    //add to articles
+    $item->sorted_tags = $tags;
+
+
 }
 
-arsort($concepts);
 
-    print "<pre>";
-    print_r($concepts);
-    print "</pre>";
+file_put_contents("/home/nick/public_html/StarHealth/assets/feed.json", json_encode(array("nodes"=>$items)));
 
 
 function sub_exists($text, $array){
@@ -109,12 +121,8 @@ function fuzzy_match($input, $words){
         }
     }
 
-    echo "Input word: $input\n";
-
     if ($shortest == 0) {
-        echo "Exact match found: $closest\n";
     } else {
-        echo "Did you mean: $closest?\n" . " +".$shortest;
     }
 
 }
