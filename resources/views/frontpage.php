c4907b86480fe0487a5f61b22b3cc18e835ad185
Nick Hargreaves <hargreaves1234@gmail.com>
2015-09-07 18:57:39 +0300

    apply material design to app cards

--- resources/views/frontpage.php
+++ resources/views/frontpage.php
@@ -127,21 +127,27 @@
             </div><!--/.nav-collapse -->
         </div><!--/.container-fluid -->
     </nav>
-    <div class="row">
+    <div class="row" id="apps">
         <div class="col-md-4">
+            <div class="app">
             <h2>Heading</h2>
             <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
             <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+                </div>
         </div>
         <div class="col-md-4">
+            <div class="app">
             <h2>Heading</h2>
             <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
             <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+            </div>
         </div>
         <div class="col-md-4">
+            <div class="app">
             <h2>Heading</h2>
             <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
             <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+            </div>
         </div>
     </div>
 
