b796a083008f4d0f06d405af82b93bfc3ff3021e
Nick Hargreaves <hargreaves1234@gmail.com>
2015-09-07 23:08:08 +0300

    footer styling

--- resources/views/frontpage.php
+++ resources/views/frontpage.php
@@ -557,7 +557,7 @@
     <!-- begin feed -->
     <div class="row news_feed">
         <div class="col-md-3 sidebar_widget2">
-            <div class="row-header"><h4>Major Stories</h4></div>
+            <div class="row-header"><h3>Major Stories</h3></div>
             <style type="text/css">
                 .accordion-inner a{
                     color:#000;
@@ -666,7 +666,7 @@
             </table>
         </div>
         <div class="col-md-6 newsfeed" id="filtered">
-            <div class="row-header"><h4>Other Health News</h4></div>
+            <div class="row-header"><h3>Other Health News</h3></div>
             <h6>A round-up of the all the latest health news from theStar <i class="icon-arrow-down" style="margin-left: 10px"></i></h6>
             <br />
             <?php
@@ -699,16 +699,16 @@
                 ?>
             </div>
         </div>
-        <div class="col-md-3 sidebar_widget2">
-            <div class="row-header"><h4>App Store</h4></div>
+        <div class="col-md-3 sidebar_widget2 links">
+            <div class="row-header"><h3>Links</h3></div>
             <p>Download the Star's mobile Apps, eBooks, and other tools.</p>
-            <a href="https://play.google.com/store/apps/details?id=org.codeforafrica.starreports" target="_blank"><img src="<?php echo asset('');?>img/android-icon.png"></a>
+            <a href="https://play.google.com/apps/testing/org.codeforafrica.citizenreporter.starreports" target="_blank"><img src="<?php echo asset('');?>img/android-icon.png"></a>
             <hr />
             <a href="http://code4kenya.org" target="_blank"><img style="height: 80px" src="<?php echo asset('');?>img/c4k_logo.png" id="c4k_partner"></a>
             <br />
             The data driven journalism + tools in StarHealth section were kickstarted by Code4Kenya
             <hr />
-            <a href="http://github.com/CodeForAfrica"><img src="<?php echo asset(''); ?>img/GitHub-Mark-32px.png" id="cfa_icon"></a>
+            <a href="http://github.com/CodeForAfrica/HealthTools"><img src="<?php echo asset(''); ?>img/GitHub-Mark-32px.png" id="cfa_icon"></a>
             <a href="http://africaopendata.org/dataset?q=kenya+health"><img style="height:32px;margin-left:25px" src="<?php echo asset(''); ?>img/data.png"></a>
             <p>The code & data for this page are open source. You can re-use it by visiting the above repositories.</p>
             <br />
@@ -723,6 +723,220 @@
             <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
         </div>
     </div>
+
+    <footer class="site-footer clearrfix row-fluid" role="contentinfo">
+
+        <div class="footer-wrapper clearfix">
+            <div class="row" id="footer">
+
+                <div class="col-md-12">
+                    <div class="row footer_inner">
+
+                        <div class="col-md-2"><!-- Bottom Menu -->
+                            <div class="bcolumn">
+                                <div class="region region-footer-1">
+                                    <div id="block-block-6" class="block block-block block-even">
+
+
+                                        <div class="content">
+                                            OUR NETWORK
+                                            <ul class="list-unstyled"><li><a href="http://www.the-star.co.ke/sections/news#overlay-context=">News</a></li>
+                                                <li><a href="http://www.the-star.co.ke/sections/regional-news#overlay-context=">Politics</a></li>
+                                                <li><a href="http://www.the-star.co.ke/sections/business#overlay-context=">Business</a></li>
+                                                <li><a href="http://www.the-star.co.ke/sections/entertainment#overlay-context=">Entertainment</a></li>
+                                                <li><a href="http://www.the-star.co.ke/sections/opinions#overlay-context=">Opinions</a></li>
+                                                <li><a href="http://www.the-star.co.ke/sections/starlife#overlay-context=">Starlife</a></li>
+                                                <li><a href="http://www.the-star.co.ke/sections/weekend-star#overlay-context=">Weekend</a></li>
+                                            </ul>  </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /Bottom Menu -->
+
+                        <div class="col-md-2 hide-mobile"><!-- Bottom Menu -->
+                            <div class="bcolumn">
+                                <div class="region region-footer-2">
+                                    <div id="block-block-7" class="block block-block hide-mobile block-odd">
+
+
+                                        <div class="content">
+                                            PARTNER SITES
+                                            <ul class="list-unstyled"><li><a href="https://classic105.com/">CLASSIC FM</a></li>
+                                                <li><a href="http://www.kissfm.co.ke/">KISS 100 FM</a></li>
+                                                <li><a href="https://mpasho.co.ke/">MPASHO</a></li>
+                                                <li><a href="#">X FM</a></li>
+                                                <li><a href="#">RADIO JAMBO</a></li>
+                                                <li><a href="http://eastfm.com/">EAST FM</a></li>
+                                            </ul>  </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /Bottom Menu -->
+
+                        <div class="col-md-2 hide-mobile"><!-- Bottom Menu -->
+                            <div class="bcolumn">
+                                <div class="region region-footer-3">
+                                    <div id="block-block-8" class="block block-block hide-mobile block-even">
+
+
+                                        <div class="content">
+                                            ONLINE SERVICES<ul class="list-unstyled"><li><a href="#">Advertise Online</a></li><li><a href="http://www.thestarepaper.co.ke/">Subscribe to Newsletter</a></li><li><a href="https://play.google.com/store/apps/details?id=ke.co.thestar.android.news&amp;hl=en">Mobile</a></li><li><a href="http://www.the-star.co.ke/the-star-feed.xml" target="_blank">RSS Feeds</a></li></ul>  </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /Bottom Menu -->
+
+                        <div class="col-md-2 hide-mobile"><!-- Bottom Menu -->
+                            <div class="bcolumn">
+                                <div class="region region-footer-4">
+                                    <div id="block-block-9" class="block block-block hide-mobile block-odd">
+
+
+                                        <div class="content">
+                                            PRINT SERVICES
+                                            <ul class="list-unstyled"><li><a href="#">Advertise in print</a></li>
+                                                <li><a href="#">Subscription Services</a></li>
+                                                <li><a href="http://member.thestarepaper.co.ke/signup">e-paper</a></li>
+                                            </ul>  </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /Bottom Menu -->
+
+                        <div class="col-md-2 hide-mobile"><!-- Bottom Menu -->
+                            <div class="bcolumn">
+                                <div class="region region-footer-5">
+                                    <div id="block-block-10" class="block block-block hide-mobile block-even">
+
+
+                                        <div class="content">
+                                            SOCIAL NETWORK
+                                            <ul class="list-unstyled"><li><a href="https://www.facebook.com/thestarkenya">Facebook Page</a></li>
+                                                <li><a href="https://twitter.com/TheStarKenya?lang=en">Twitter</a></li>
+                                                <li><a href="https://plus.google.com/u/0/102746437663153160461/posts">Google +</a></li>
+                                                <li><a href="http://instagram.com/thestarkenya">Instagram Page</a></li>
+                                            </ul>  </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /Bottom Menu -->
+
+                        <div class="col-md-2 hide-mobile"><!-- Bottom Menu -->
+                            <div class="bcolumn">
+                                <div class="region region-footer-6">
+                                    <div id="block-block-11" class="block block-block hide-mobile block-odd">
+
+
+                                        <div class="content">
+                                            CONTACT US
+                                            <ul class="list-unstyled"><li><a href="http://www.the-star.co.ke/section/contact-details">Contact Details</a></li>
+
+                                            </ul>
+                                            OTHER
+                                            <ul class="list-unstyled"><li><a href="http://www.the-star.co.ke/section/sitemap">Sitemap</a></li>
+                                                <li><a href="http://www.the-star.co.ke/section/website-comment-policy">Website Comment Policy</a></li>
+                                                <li><a href="#">Terms &amp; Conditions</a></li>
+                                                <li><a href="#">Ethics - Policy</a></li>
+                                                <li><a href="#">Register of interests</a></li>
+                                            </ul>  </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /Bottom Menu -->
+
+                    </div>
+                </div>
+
+                <div class="col-md-12 text-center" id="copyright"><!-- copyright -->
+
+
+                    <div class="credit">
+                        <a href="http://code4kenya.org" target="_blank"><span class="credit-text">Built by </span>
+                            <img src="<?php echo asset('');?>img/c4k_logo.png"></a>
+                    </div>
+
+                    <div class="region region-footer-bottom">
+                        <div id="block-block-5" class="block block-block block-even">
+
+
+                            <div class="content">
+                                © The Star. All Rights Reserved
+
+                                Use of this site constitutes acceptance of our <a href="#">Terms and Conditions</a> and <a href="#">Privacy Policy</a>.  </div>
+                        </div>
+                        <div id="block-block-43" class="block block-block block-odd">
+
+
+                            <div class="content">
+                                <script>
+                                    (function(f,b){
+                                        var c;
+                                        f.hj=f.hj||function(){(f.hj.q=f.hj.q||[]).push(arguments)};
+                                        f._hjSettings={hjid:23751, hjsv:3};
+                                        c=b.createElement("script");c.async=1;
+                                        c.src="//static.hotjar.com/c/hotjar-23751.js?sv=3";
+                                        b.getElementsByTagName("head")[0].appendChild(c);
+                                    })(window,document);
+                                </script>
+                            </div>
+                        </div>
+                        <div id="block-block-44" class="block block-block block-even">
+
+
+                            <div class="content">
+                                <!--StarReports Analytics-->
+                                <script>
+                                    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                                            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                                        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                                    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+                                    ga('create', 'UA-44795600-11', 'auto');
+                                    ga('create', 'UA-21433057-1', 'auto', {'name': 'theStar'});
+                                    ga('create', 'UA-21433057-5', 'auto', {'name': 'theStarHealth'});
+                                    ga('create', 'UA-33350783-4', 'auto', {'name': 'CfAFRICA'});
+                                    ga('send', 'pageview');
+                                    ga('theStar.send', 'pageview');
+                                    ga('theStarHealth.send', 'pageview');
+                                    ga('CfAFRICA.send', 'pageview');
+
+                                </script>
+                            </div>
+                        </div>
+                        <div id="block-block-49" class="block block-block block-odd">
+
+
+                            <div class="content">
+                                <script>
+                                    window.fbAsyncInit = function() {
+                                        FB.init({
+                                            appId      : '582026265221270',
+                                            xfbml      : true,
+                                            version    : 'v2.3'
+                                        });
+                                    };
+
+                                    (function(d, s, id){
+                                        var js, fjs = d.getElementsByTagName(s)[0];
+                                        if (d.getElementById(id)) {return;}
+                                        js = d.createElement(s); js.id = id;
+                                        js.src = "//connect.facebook.net/en_US/sdk.js";
+                                        fjs.parentNode.insertBefore(js, fjs);
+                                    }(document, 'script', 'facebook-jssdk'));
+                                </script>  </div>
+                        </div>
+                    </div>
+                    <!-- /copyright -->
+                </div>
+            </div><!-- footer-wrapper-->
+        </div>
+    </footer>
 </div><!-- /.container -->
 
 
