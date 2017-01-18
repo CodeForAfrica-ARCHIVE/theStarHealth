e36ff198230ee13244998a10bc8781b3460ad131
David Lemayian <david@on.co.ke>
2017-01-18 16:10:58 +0300

    Remove var_dump.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -36,8 +36,6 @@ Route::get('sms', function()
 
     $result = (new SMSController())->process_received($phone, $message);
 
-    var_dump($result);
-
     $result = (new SMSController())->send_response($phone, $result);
 
     //return $result;
